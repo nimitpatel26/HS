176e0dc752bf4e8dc168799eed9a3282b4ba370c
root <np9@umbc.edu>
2019-04-30 14:35:08 +0000

    second

--- HS/settings.py
+++ HS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nrx1%(*yfa3e!s91w)#ween4rm9h8c3bp-#v_52%zd8hvd3h+s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['165.22.252.29', 'localhost', '.github.com/nimitpatel26/HS']
 
 
 # Application definition
@@ -73,6 +73,7 @@ WSGI_APPLICATION = 'HS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
